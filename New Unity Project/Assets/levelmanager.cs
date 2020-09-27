a8a4bbe53c3391bb238124edeacf12a6941a2d1c
theevik <viktorerko2000@gmail.com>
2020-09-27 18:37:25 +0200

    emergent game, just testing

---
+++ New Unity Project/Assets/levelmanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelmanager : MonoBehaviour
+{
+    public static levelmanager instance;
+
+       
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+     public void Respawn ()
+    {
+        Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+    }
+}
---
+++ New Unity Project/Assets/scriptintro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class scriptintro : MonoBehaviour
+{
+
+    public float Speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(-4f, 0.5f);
+    }
+
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
+    }
+
+
+}
