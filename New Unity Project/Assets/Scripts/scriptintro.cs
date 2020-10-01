e5bb2a4de07e959cd70d5a04d13a17e521ff2329
Jennefleur <70879216+Jennefleur@users.noreply.github.com>
2020-10-01 14:52:32 +0200

    Field Of View

--- New Unity Project/Assets/Scripts/scriptintro.cs
+++ New Unity Project/Assets/Scripts/scriptintro.cs
@@ -1,36 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
-using System.Threading;
 using UnityEngine;
 
 public class scriptintro : MonoBehaviour
 {
+    [SerializeField]
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+    [SerializeField]
+    private float speed = 2;
 
-    public float Speed = 5f;
-    public Rigidbody2D rb;
-    Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(-4f, 0.5f);
+
     }
-
-    
-
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        rigidbody.velocity = new Vector2(horizontal, vertical) * speed;
+        var totalSpeed = rigidbody.velocity.magnitude;
+
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
-    }
-
-
 }
