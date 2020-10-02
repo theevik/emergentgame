a4c0bca7d6ba99bb716d06aa8ff16e73b641f205
sanderennema <34135986+sanderennema@users.noreply.github.com>
2020-10-02 11:46:54 +0200

    SceneLoading map01 to map02

---
+++ New Unity Project/Assets/Scripts/MoveToNextScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextScene : MonoBehaviour
+{
+    public GameObject player;
+
+    public int LoadNextOrPrevious = 1;
+
+    private int nextSceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + LoadNextOrPrevious;
+        player.gameObject.GetComponent<scriptintro>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision = player.GetComponent<Collider2D>();
+
+        if(collision)
+        {
+            SceneManager.LoadScene(nextSceneToLoad);
+        }
+        
+    }
+   
+}
