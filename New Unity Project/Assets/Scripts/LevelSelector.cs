4e4d4ee292b96d302f38e7f6c5c348e7a1fe5381
sanderennema <34135986+sanderennema@users.noreply.github.com>
2020-10-03 13:19:37 +0200

    Basic Main Menu

---
+++ New Unity Project/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void Load01()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Load02()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Load03()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
