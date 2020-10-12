e715b65ad645ea34871750bcdb984ae7c3a7c6c1
sanderennema <34135986+sanderennema@users.noreply.github.com>
2020-10-12 15:43:27 +0200

    Newest version

---
+++ New Unity Project/Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+
+    public void RestarGame()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
